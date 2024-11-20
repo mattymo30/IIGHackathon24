304649ed914019b9ab4514811bdea81458f9a6ff
mattymo30 <msmorrison39@yahoo.com>
2024-11-20 23:40:35 +0000

    Remove unused imports

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import {FormsModule} from '@angular/forms';
 import { RepoService } from "./repo.service";
 import { AppComponent } from "./app.component";
 
 @NgModule({
     declarations: [
         AppComponent,
+        RepoService,
     ],
     imports: [
         BrowserModule,
-        FormsModule
 
     ],
-    providers: [RepoService,],
+    providers: [],
     bootstrap: [AppComponent]
 })
 export class AppModule {}
